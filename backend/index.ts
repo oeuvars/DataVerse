ec3612f920ed715157f705c7efb953c6fa64a762
Oeuvars <www.anuragniall@gmail.com>
2023-12-19 22:58:18 +0530

    Backend folder structure updated

--- backend/index.ts
+++ backend/index.ts
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import 'dotenv/config';
-import { router } from './routes/userRoutes';
+import { router } from './src/routes/userRoutes';
 const app = express();
 
 const PORT = process.env.PORT || 5000
